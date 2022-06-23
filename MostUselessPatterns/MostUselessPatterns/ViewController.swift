71186fa7cb38f2a012e83bcdeb4e2eb25d772de8
Grigory <grishavinyar@icloud.com>
2022-06-23 15:37:57 +0300

    Create a project

---
+++ MostUselessPatterns/MostUselessPatterns/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MostUselessPatterns
+//
+//  Created by Григорий Виняр on 23/06/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemPink
+    }
+
+
+}
+
