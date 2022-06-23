dbd68f03894905e1fda479abed0781e465c1fb5c
Grigory <grishavinyar@icloud.com>
2022-06-23 16:29:19 +0300

    Used Factory pattern in project

--- MostUselessPatterns/MostUselessPatterns/ViewController.swift
+++ MostUselessPatterns/MostUselessPatterns/ViewController.swift
@@ -9,9 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var alertFacotry = DefaultAlertsFactory()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemPink
+        
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        let alert = alertFacotry.getAlert(by: .unexpectedErrorAlert)
+        self.present(alert, animated: true)
     }
 
 
